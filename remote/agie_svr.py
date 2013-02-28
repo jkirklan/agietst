e2604615aa8acf9ab2f49075a30bb1370c40d434
James Kirkland <jkirklan@redhat.com>
2013-02-28 14:14:10 -0500

    mv drain.py to agie_svr.py and set up funct to split up and down in agie_svr

--- remote/agie_svr.py
+++ remote/agie_svr.py
@@ -6,6 +6,12 @@ from qpid.messaging import *
 broker_local = "localhost:5672"
 addr_control = "agie_inbound/agie_inbound_control"
 
+
+def intf_up(foo):
+	print ' '
+
+
+
 def broker_conn():
 # create connection to local broker
         lb_connection = Connection(broker_local)
@@ -15,7 +21,15 @@ def broker_conn():
 		receiver = session.receiver("agie_inbound_control")
 		while True:
 			message = receiver.fetch()
-			print message.content
+			received = message.content
+			msg_list = received.split(',')	
+			print msg_list
+			if msg_list[0] == 'up':
+				print "Uppy"
+			elif msg_list[0] == 'down':
+				print "Downer"
+			else:
+				print "freakout"
 			session.acknowledge()
         except MessagingError,m:
                 print m
